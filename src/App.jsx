dd93e0dc7110143baa2048d256dc1cc6efd298fa
lukke armageddon <38384344+lukkearmageddon@users.noreply.github.com>
2024-02-18 09:26:52 +0100

    init: start new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <>
+      <h1> header</h1>
+      <div>Проект</div>
+      <Button />
+    </>
+  );
+}
+
+export default App;
