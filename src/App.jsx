9dce746c7e22c7aa116c71259e647b8884d647e8
lukkearmageddon <lukkemail@gmail.com>
2024-02-24 12:06:21 +0100

    feat: rewrite logic in journalForm with reducer

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,51 @@ import Body from './Layout/Body/Body';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const INITIAL_DATA = [
-	// {
-	// 	id: 1,
-	// 	title: 'Подготовка к обновлению курсов',
-	// 	text: 'Горные походы открывают удивительные природные ландшафты',
-	// 	date: new Date()
-	// },
-	// {
-	// 	id: 2,
-	// 	title: 'Поход в горы',
-	// 	text: 'Тут должен быть другой текст, но его нет.',
-	// 	date: new Date()
-	// }
-];
+// const INITIAL_DATA = [
+// 	{
+// 		"id": "1",
+// 		"title": "Подготовка к обновлению курсов",
+// 		"text": "Горные походы открывают удивительные природные ландшафты",
+// 		"tag": "tag",
+// 		"date": "2024/07/32"
+// 	},
+// 	{
+// 		"id": "2",
+// 		"title": "Поход в горы",
+// 		"text": "Тут должен быть другой текст, но его нет.",
+// 		"tag": "tag",
+// 		"date": "2024/07/32"
+// 	}
+// ]
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (items.length) {
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
+
 	const addItem = item => {
 		setItems(prev => [...prev, {
-			text: item.text,
+			id: prev.length > 0 ? Math.max(...prev.map(i => i.id)) + 1 : 1,
 			title: item.title,
 			date: new Date(item.date),
-			id: prev.length > 0 ? Math.max(...prev.map(i => i.id)) + 1 : 1
+			tag: item.tag,
+			text: item.post
 		}]);
 	};
 
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,74 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import Button from '../Button/Button';
 import styles from './JournalForm.module.css';
 import cn from 'classnames';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 export default function JournalForm({ onSubmit }) {
-	const [formValidState, setFormValidState] = useState({
-		title: true,
-		post: true,
-		date: true
-	});
+
+	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const {isValid, isFormReadyToSubmit, values} = formState;
+
+	useEffect(() => {
+		let timerId;
+		if (!isValid.date || !isValid.post || !isValid.title) {
+			timerId = setTimeout(() => {
+				dispatchForm({ type: 'RESET_VALIDITY' });
+			}, 2000);
+		}
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isValid]);
+
+	useEffect(() => {
+		if (isFormReadyToSubmit) {
+			onSubmit(values);
+		}
+	}, [isFormReadyToSubmit]);
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		let isFormValid = true;
-
-		if (!formProps.title?.trim().length) {
-			setFormValidState(state => ({ ...state, title: false }));
-			isFormValid = false;
-		} else {
-			setFormValidState(state => ({ ...state, title: true }));
-		}
-		if (!formProps.post?.trim().length) {
-			setFormValidState(state => ({ ...state, post: false }));
-			isFormValid = false;
-		} else {
-			setFormValidState(state => ({ ...state, post: true }));
-		}
-		if (!formProps.date) {
-			setFormValidState(state => ({ ...state, date: false }));
-			isFormValid = false;
-		} else {
-			setFormValidState(state => ({ ...state, date: true }));
-		}
-		if (!isFormValid) {
-			return;
-		}
-		onSubmit(formProps);
+		dispatchForm({ type: 'SUBMIT', payload: formProps });
 	};
 
 	return (
 		<form className={cn(styles['journal-form'])} onSubmit={addJournalItem}>
 			<div>
 				<input type="text" name='title' className={cn(styles['input-title'], {
-					[styles['invalid']]: !formValidState.title
+					[styles['invalid']]: !isValid.title
 				})} />
 			</div>
 			<div className={styles['form-row']}>
-				<label className={styles['form-label']} for="date">
+				<label className={styles['form-label']} htmlFor="date">
 					<img src="/src/assets/date.svg" alt="date icon" />
 					<span>Date</span>
 				</label>
 				<input type="date" name='date' id="date" className={cn(styles['input'], {
-					[styles['invalid']]: !formValidState.date
+					[styles['invalid']]: !isValid.date
 				})} />
 			</div>
 			<div className={styles['form-row']}>
-				<label className={styles['form-label']} for="tag">
+				<label className={styles['form-label']} htmlFor="tag">
 					<img src="/src/assets/folder.svg" alt="folder icon" />
 					<span>Tag</span>
 				</label>
-				<input type="text" name='tag' id='tag' className={cn(styles['input'])}/>
+				<input type="text" name='tag' id='tag' className={cn(styles['input'])} />
 			</div>
 			<textarea
 				name='post'
 				id='post'
 				cols="30"
 				rows='10'
-				className={cn(styles['input'], {[styles['invalid']]: !formValidState.post})}
+				className={cn(styles['input'], { [styles['invalid']]: !isValid.post })}
 			></textarea>
 			<Button text="Save" />
 		</form>
